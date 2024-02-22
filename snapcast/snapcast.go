b2567a7bedc837821bda371526abddf5b7e2ea92
ConnorsApps <ConnorSkees@icloud.com>
2024-02-22 18:35:20 -0500

    renaming

--- snapcast/snapcast.go
+++ snapcast/snapcast.go
@@ -36,21 +36,23 @@ func (s StreamStatus) IsIdle() bool {
 	return s == StreamIdle
 }
 
-type Error struct {
-	Code    int         `json:"code,omitempty"`
-	Data    interface{} `json:"data,omitempty"`
-	Message string      `json:"message,omitempty"`
-}
+type (
+	Error struct {
+		Code    int         `json:"code,omitempty"`
+		Data    interface{} `json:"data,omitempty"`
+		Message string      `json:"message,omitempty"`
+	}
 
-type Message struct {
-	ID         *int        `json:"id,omitempty"`
-	JsonRPC    string      `json:"jsonrpc,omitempty"`
-	Method     *Method     `json:"method,omitempty"`
-	Error      *Error      `json:"error,omitempty"`
-	Result     interface{} `json:"result,omitempty"`
-	Params     interface{} `json:"params,omitempty"`
-	ReceivedAt time.Time   `json:"-"`
-}
+	Message struct {
+		ID         *int        `json:"id,omitempty"`
+		JsonRPC    string      `json:"jsonrpc,omitempty"`
+		Method     *Method     `json:"method,omitempty"`
+		Error      *Error      `json:"error,omitempty"`
+		Result     interface{} `json:"result,omitempty"`
+		Params     interface{} `json:"params,omitempty"`
+		ReceivedAt time.Time   `json:"-"`
+	}
+)
 
 func ParseResult[T any](result interface{}) (*T, error) {
 	var t = new(T)
@@ -64,75 +66,77 @@ func ParseResult[T any](result interface{}) (*T, error) {
 	return t, err
 }
 
-type Stream struct {
-	ID     string       `json:"id"`
-	Status StreamStatus `json:"status"`
-	URI    struct {
-		Fragment string            `json:"fragment"`
-		Host     string            `json:"host"`
-		Path     string            `json:"path"`
-		Query    map[string]string `json:"query"`
-		Raw      string            `json:"raw"`
-		Scheme   string            `json:"scheme"`
-	} `json:"uri"`
-}
+type (
+	Stream struct {
+		ID     string       `json:"id"`
+		Status StreamStatus `json:"status"`
+		URI    struct {
+			Fragment string            `json:"fragment"`
+			Host     string            `json:"host"`
+			Path     string            `json:"path"`
+			Query    map[string]string `json:"query"`
+			Raw      string            `json:"raw"`
+			Scheme   string            `json:"scheme"`
+		} `json:"uri"`
+	}
 
-type Snapserver struct {
-	ControlProtocolVersion int    `json:"controlProtocolVersion"`
-	Name                   string `json:"name"`
-	ProtocolVersion        int    `json:"protocolVersion"`
-	Version                string `json:"version"`
-}
+	Snapserver struct {
+		ControlProtocolVersion int    `json:"controlProtocolVersion"`
+		Name                   string `json:"name"`
+		ProtocolVersion        int    `json:"protocolVersion"`
+		Version                string `json:"version"`
+	}
 
-type Volume struct {
-	Muted   bool `json:"muted"`
-	Percent int  `json:"percent"`
-}
+	Volume struct {
+		Muted   bool `json:"muted"`
+		Percent int  `json:"percent"`
+	}
 
-type Client struct {
-	Config struct {
-		Instance int    `json:"instance"`
-		Latency  int    `json:"latency"`
-		Name     string `json:"name"`
-		Volume   Volume `json:"volume"`
-	} `json:"config"`
-	Connected bool `json:"connected"`
-	Host      struct {
-		Arch string `json:"arch"`
-		IP   string `json:"ip"`
-		MAC  string `json:"mac"`
-		Name string `json:"name"`
-		OS   string `json:"os"`
-	} `json:"host"`
-	ID       string `json:"id"`
-	LastSeen struct {
-		Sec  int `json:"sec"`
-		USec int `json:"usec"`
-	} `json:"lastSeen"`
-	Snapclient struct {
-		Name            string `json:"name"`
-		ProtocolVersion int    `json:"protocolVersion"`
-		Version         string `json:"version"`
-	} `json:"snapclient"`
-}
+	Client struct {
+		Config struct {
+			Instance int    `json:"instance"`
+			Latency  int    `json:"latency"`
+			Name     string `json:"name"`
+			Volume   Volume `json:"volume"`
+		} `json:"config"`
+		Connected bool `json:"connected"`
+		Host      struct {
+			Arch string `json:"arch"`
+			IP   string `json:"ip"`
+			MAC  string `json:"mac"`
+			Name string `json:"name"`
+			OS   string `json:"os"`
+		} `json:"host"`
+		ID       string `json:"id"`
+		LastSeen struct {
+			Sec  int `json:"sec"`
+			USec int `json:"usec"`
+		} `json:"lastSeen"`
+		Snapclient struct {
+			Name            string `json:"name"`
+			ProtocolVersion int    `json:"protocolVersion"`
+			Version         string `json:"version"`
+		} `json:"snapclient"`
+	}
 
-type Group struct {
-	Clients  []Client `json:"clients"`
-	ID       string   `json:"id"`
-	Muted    bool     `json:"muted"`
-	Name     string   `json:"name"`
-	StreamID string   `json:"stream_id"`
-}
+	Group struct {
+		Clients  []Client `json:"clients"`
+		ID       string   `json:"id"`
+		Muted    bool     `json:"muted"`
+		Name     string   `json:"name"`
+		StreamID string   `json:"stream_id"`
+	}
 
-type Server struct {
-	Groups []Group `json:"groups"`
-	Host   struct {
-		Arch string `json:"arch"`
-		IP   string `json:"ip"`
-		MAC  string `json:"mac"`
-		Name string `json:"name"`
-		OS   string `json:"os"`
-	} `json:"host"`
-	Snapserver Snapserver `json:"snapserver"`
-	Streams    []Stream   `json:"streams"`
-}
+	Server struct {
+		Groups []Group `json:"groups"`
+		Host   struct {
+			Arch string `json:"arch"`
+			IP   string `json:"ip"`
+			MAC  string `json:"mac"`
+			Name string `json:"name"`
+			OS   string `json:"os"`
+		} `json:"host"`
+		Snapserver Snapserver `json:"snapserver"`
+		Streams    []Stream   `json:"streams"`
+	}
+)
--- snapclient/handlers.go
+++ snapclient/handlers.go
@@ -1,4 +1,4 @@
-package client
+package snapclient
 
 import (
 	"encoding/json"
